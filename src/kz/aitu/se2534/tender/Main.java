953c9340bf66e75525f01b1efc7085a3c6fa5acb
Maksat <aylbekov9@gmail.com>
2026-09-10 13:39:15 +0500

    feat: Add TenderDirector and Main client implementation
    Implemented TenderDirector with polymorphic build configurations using the TenderBuilder interface. Added Main class to demonstrate building both WebTender and TenderSpecification representations.

--- src/kz/aitu/se2534/tender/Main.java
+++ src/kz/aitu/se2534/tender/Main.java
@@ -2,6 +2,22 @@ package kz.aitu.se2534.tender;
 
 public class Main {
     static void main(String[] args) {
+        TenderDirector director = new TenderDirector();
 
+        WebTenderBuilder WebBuilder = new WebTenderBuilder();
+        director.makeAgroTender(WebBuilder);
+        WebTender agroTender = WebBuilder.getResult();
+
+        System.out.println("--- Representation 1: Java Object ---");
+        System.out.println(agroTender);
+
+        System.out.println();
+
+        TenderSpecificationBuilder specBuilder = new TenderSpecificationBuilder();
+        director.makeAgroTender(specBuilder);
+        TenderSpecification specSheet = specBuilder.getResult();
+
+        System.out.println("--- Representation 2: Tender Specification ---");
+        System.out.println(specSheet);
     }
 }
--- src/kz/aitu/se2534/tender/TenderDirector.java
+++ src/kz/aitu/se2534/tender/TenderDirector.java
@@ -1,4 +1,18 @@
 package kz.aitu.se2534.tender;
 
 public class TenderDirector {
+
+    public void makeAgroTender(TenderBuilder builder) {
+        builder.setCategory("Agroculture")
+                .setBudget(50000.0)
+                .setRegion("Akmola Region")
+                .setDescription("Supply of combine harvesters and wheat seeds");
+    }
+
+    public void makeConstructionTender(TenderSpecificationBuilder builder) {
+        builder.setCategory("Construction")
+                .setBudget(100000.0)
+                .setRegion("Almaty Region")
+                .setDescription("Construction of a new office building");
+    }
 }

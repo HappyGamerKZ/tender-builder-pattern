635dc526af9f7915a525c83fb5f31f93eab7d418
Maksat <aylbekov9@gmail.com>
2026-09-10 13:07:53 +0500

    feat: Implement TenderSpecification domain product
    Defined immutable TenderSpecification class with package-private constructor, final fields, and getters to strictly comply with encapsulation requirements.

--- src/kz/aitu/se2534/tender/TenderSpecification.java
+++ src/kz/aitu/se2534/tender/TenderSpecification.java
@@ -1,4 +1,16 @@
 package kz.aitu.se2534.tender;
 
-public class TenderSpecification {
+public final class TenderSpecification {
+    private final String content;
+
+    TenderSpecification(String content) {
+        this.content = content;
+    }
+
+    public String getContent() { return content; }
+
+    @Override
+    public String toString() {
+        return content;
+    }
 }

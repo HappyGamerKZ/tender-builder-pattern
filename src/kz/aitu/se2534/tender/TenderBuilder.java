6c5b902fe0af4fd323c368ba78adba2e5d114adc
Maksat <aylbekov9@gmail.com>
2026-09-10 13:11:02 +0500

    feat: Add TenderBuilder interface
    Defined generic Builder interface declaring fluent methods for setting budget, region, category, and description.

--- src/kz/aitu/se2534/tender/TenderBuilder.java
+++ src/kz/aitu/se2534/tender/TenderBuilder.java
@@ -1,4 +1,8 @@
 package kz.aitu.se2534.tender;
 
 public interface TenderBuilder {
+    TenderBuilder setBudget(double budget);
+    TenderBuilder setRegion(String region);
+    TenderBuilder setCategory(String category);
+    TenderBuilder setDescription(String description);
 }
